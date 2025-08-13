e22bf444e1e9d444bda20855bc9cc41f88c6a5de
camilalazarozubillaga <furicafurica697@gmail.com>
2025-08-13 19:15:18 -0300

    CL - changes to the login and challenge

---
+++ js/login.js
@@ -0,0 +1,17 @@
+document
+  .getElementById('loginForm')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    if (username !== '' && password !== '') {
+      localStorage.setItem('loggedIn', 'true');
+      localStorage.setItem('username', username);
+
+      window.location.assign('index.html');
+    } else {
+      alert('Todos los campos deben estar llenos');
+    }
+  });
