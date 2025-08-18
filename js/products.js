79e67828edd93c71bd4638512fb865409061ccf4
santiago-gonzalez-gutierrez <santigongu@gmail.com>
2025-08-18 15:21:41 -0300

    Agrego filtros en products

--- js/products.js
+++ js/products.js
@@ -1,15 +1,28 @@
 const productos_url = 'https://japceibal.github.io/emercado-api/cats_products/101.json'
 let currentProductsArray = [];
 
+function sortProductsDesc(dataArray){
+    dataArray.products.sort((a,b) => a.cost - b.cost)
+}
+function sortProductsAsc(dataArray){
+    dataArray.products.sort((a,b) => b.cost - a.cost)
+}
+function sortProductsSell(dataArray){
+    dataArray.products.sort((a,b) => b.soldCount - a.soldCount)
+}
+
+function showTitle(title){
+    let htmlContentToAppend = `
+         <div class="titulo">
+            <h1>${title}</h1>
+        </div>
+    `;
+      document.getElementById("titulo").innerHTML = htmlContentToAppend;
+}
 
 function mostrarProductos(dataArray) {
     let htmlContentToAppend = "";
     let products = dataArray.products;
-    htmlContentToAppend += `
-        <div class="titulo">
-            <h1>${dataArray.catName}</h1>
-        </div>
-    `;
     for (let product of products) {
         htmlContentToAppend += `
             <div class="product-list-item">
@@ -30,16 +43,32 @@ function mostrarProductos(dataArray) {
         `;
     }
 
-    document.getElementById("productos-lista").innerHTML = htmlContentToAppend;
+    document.getElementById("list").innerHTML = htmlContentToAppend;
 }
+
+
 
 
 document.addEventListener("DOMContentLoaded", function(e){
  getJSONData(productos_url).then(function(resultObj){
      if (resultObj.status === "ok"){
         currentProductsArray = resultObj.data
+        showTitle(resultObj.data.catName)
+        sortProductsDesc(currentProductsArray); 
         mostrarProductos(currentProductsArray)
         //sortAndShowCategories(ORDER_ASC_BY_NAME, resultObj.data);
         }
     });
+
+    document.getElementById("miCombobox").addEventListener("change",function(event){
+        let valorSeleccionado = event.target.value;    
+        if(valorSeleccionado === "desc"){
+           sortProductsDesc(currentProductsArray); 
+        }else if(valorSeleccionado === "asc"){
+            sortProductsAsc(currentProductsArray);
+        }else{
+           sortProductsSell(currentProductsArray); 
+        }
+        mostrarProductos(currentProductsArray);
+    })    
 })
