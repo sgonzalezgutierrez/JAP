ec9144194b08abe002d576b2d864c5602657a1ce
agustindavyt <agusdavyt@gmail.com>
2025-09-11 16:25:41 -0300

    parte 4.2 agustin

--- js/product-info.js
+++ js/product-info.js
@@ -1,17 +1,16 @@
 const idSeleccionado = localStorage.getItem("productoSeleccionado");
-
-fetch("https://japceibal.github.io/emercado-api/cats_products/101.json")
+const url = `https://japceibal.github.io/emercado-api/products/${idSeleccionado}.json`;
+fetch(url)
     .then(response => response.json())
     .then(data => {
-        const producto = data.products.find(p => p.id == idSeleccionado);
-        if (producto) {
-            document.querySelector("h4").textContent = producto.name;
-            document.querySelector(".precio").textContent = `Precio: ${producto.currency} ${producto.cost}`;
-            document.querySelector(".vendidos").textContent = `Cantidad vendida: ${producto.soldCount}`;
-            document.querySelector(".descripción-info").textContent = producto.description;
-            const galeria = document.querySelector(".galeria-imagenes");
-            galeria.innerHTML = '<p class="galeria">Galería</p>';
-          
-                galeria.innerHTML += `<img class="img-thumbnail mx-2" src="${producto.image}" alt="Imagen del producto">`;
-        }
-    });
+        document.querySelector("h4").textContent = data.name;
+        document.querySelector(".precio").textContent = `Precio: ${data.currency} ${data.cost}`;
+        document.querySelector(".vendidos").textContent = `Cantidad vendida: ${data.soldCount}`;
+        document.querySelector(".descripción-info").textContent = data.description;
+        const galeria = document.querySelector(".galeria-imagenes");
+        galeria.innerHTML = '<p class="galeria">Galería</p>';
+        data.images.forEach(image => {
+            galeria.innerHTML += `<img class="img-thumbnail mx-2" src="${image}" alt="Imagen del producto">`;
+        }); //el + va agregando imagenenes y no las borra.
+    }
+    );
